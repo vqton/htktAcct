3961a5ef32a45a590f9c9040954ab546bd052771
vqton <vuquangton@outlook.com>
2022-08-02 15:03:30 +0700

    updating minimal templates

--- website/auth.py
+++ website/auth.py
@@ -1,18 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
 
 
 @auth.route("/login")
 def login():
-    return "Logged in"
+    return render_template("login.html")
 
 
 @auth.route("/logout")
 def logout():
-    return "Logged out"
+    return render_template("logout.html")
 
 
 @auth.route("/sign-up")
 def sign_up():
-    return "Signed up"
+    return render_template("sign_up.html")
