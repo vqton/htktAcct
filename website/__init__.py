69dfb1dc552ff4966cfb19c7f8f86f6ee8d836ad
vqton <vuquangton@outlook.com>
2022-08-02 11:24:51 +0700

    add template - base html

--- website/__init__.py
+++ website/__init__.py
@@ -9,5 +9,5 @@ def create_app():
     from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
-    app.register_blueprint(auth, url_prefix="/auth")
+    app.register_blueprint(auth, url_prefix="/")
     return app
--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,18 @@
 from flask import Blueprint
 
 auth = Blueprint("auth", __name__)
+
+
+@auth.route("/login")
+def login():
+    return "Logged in"
+
+
+@auth.route("/logout")
+def logout():
+    return "Logged out"
+
+
+@auth.route("/sign-up")
+def sign_up():
+    return "Signed up"
