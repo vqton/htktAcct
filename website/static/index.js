3bcaa11d5baf14aef15443ee509b631e833adb50
vqton <vuquangton@outlook.com>
2022-08-09 15:38:51 +0700

    Fix index javascript

--- website/static/index.js
+++ website/static/index.js
@@ -1,13 +1,10 @@
-function deleteNote(noteId)
-{
-    fetch("/delete-note",{
-        method:"POST",
-        body:JSON.stringify({
-            noteId: noteId
-        }).then((_res)=>{
-            window.location.href="/"; 
-        });
-    })
+function deleteNote(noteId) {
+    fetch("/delete-note", {
+        method: "POST",
+        body: JSON.stringify({ noteId: noteId }),
+    }).then((_res) => {
+        window.location.href = "/";
+    });
 }
 
 // function deleteNote(noteId) {
